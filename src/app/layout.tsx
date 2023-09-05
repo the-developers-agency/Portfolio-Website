fc57e904b2f41574de6fed2e0c33d4ac6549de2b
prynsh7 <priyanshraj45@gmail.com>
2023-09-06 00:22:31 +0530

    favicon :: fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
         />
         <meta property="og:type" content="website" />
         <meta content="summary_large_image" name="twitter:card" />
-
+        <link href="/images/logo.png" rel="shortcut icon" type="image/x-icon" />
         <link href="/images/logo.png" rel="apple-touch-icon" />
       </head>
       <body className={inter.className}>{children}</body>
